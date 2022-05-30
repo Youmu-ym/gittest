6a7efacac734e55606f7ee6313764cd239992fe6
youmu <3428261372@qq.com>
2022-05-30 16:55:15 +0800

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author: 柚木
+ * @date: 2022/5/30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+    }
+}
